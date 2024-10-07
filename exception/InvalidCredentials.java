b10f40bf5a3e37e019508f1c92e32658e18dc0bf
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-10-07 12:20:49 +0400

    Add files via upload

---
+++ exception/InvalidCredentials.java
@@ -0,0 +1,10 @@
+package org.example.authorization_service.exception;
+
+//класс является наследником RuntimeException используется
+// для того чтобы показать пользователю что имя или пароль
+// введены не правильно
+public class InvalidCredentials extends RuntimeException {
+    public InvalidCredentials(String msg) {
+        super(msg);
+    }
+}
---
+++ model/Authorities.java
@@ -0,0 +1,5 @@
+package org.example.authorization_service.model;
+
+public enum Authorities {
+    READ, WRITE, DELETE;
+}
